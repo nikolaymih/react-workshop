d81e825ecc2177751104176c41b1b1b08dac9a3c
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-03-22 08:55:20 +0200

    Categories
    make fetch with react hooks

---
+++ src/components/Categories/CategoriNavigation/CategoriNavigation.js
@@ -0,0 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
+const CategoriNavigation = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li><NavLink activeClassName="nav-link-selected" to="/categories/all">All</NavLink></li>
+                <li><NavLink activeClassName="nav-link-selected" to="/categories/cats">Cats</NavLink></li>
+                <li><NavLink activeClassName="nav-link-selected" to="/categories/dogs">Dogs</NavLink></li>
+                <li><NavLink activeClassName="nav-link-selected" to="/categories/parrots">Parrots</NavLink></li>
+                <li><NavLink activeClassName="nav-link-selected" to="/categories/reptiles">Reptiles</NavLink></li>
+                <li><NavLink activeClassName="nav-link-selected" to="/categories/other">Other</NavLink></li>
+            </ul>
+            <style jsx>{`
+                .nav-link-selected{
+                    background-color: lightgreen !important;
+                }
+            `}</style>
+        </nav>
+    );
+};
+
+export default CategoriNavigation;
---
+++ src/components/Categories/Categories.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import CategoriNavigation from './CategoriNavigation/CategoriNavigation';
+
+const Categories = ({
+    match
+}) => {
+    const [pets, setPets] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/pets')
+            .then((res) => res.json())
+            .then((data) => setPets(data))
+    }, [])
+    return (
+        <section className="dashboard">
+            <h1>Dashboard</h1>
+            <CategoriNavigation />
+            <ul className="other-pets-list">
+                <li className="otherPet">
+                    <h3>Name: Gosho</h3>
+                    <p>Category: Cat</p>
+                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" /></p>
+                    <p className="description">This is not my cat Gosho</p>
+                    <div className="pet-info">
+                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
+                        <a href="#"><button className="button">Details</button></a>
+                        <i className="fas fa-heart"></i> <span> 2</span>
+                    </div>
+                </li>
+                <li className="otherPet">
+                    <h3>Name: Gosho</h3>
+                    <p>Category: Cat</p>
+                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" /></p>
+                    <p className="description">This is not my cat Gosho</p>
+                    <div className="pet-info">
+                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
+                        <a href="#"><button className="button">Details</button></a>
+                        <i className="fas fa-heart"></i> <span> 2</span>
+                    </div>
+
+                </li>
+                <li className="otherPet">
+                    <h3>Name: Kiro</h3>
+                    <p>Category: Dog</p>
+                    <p className="img"><img src="http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bbde.png" /></p>
+                    <p className="description">This is my dog Kiro</p>
+                    <div className="pet-info">
+                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
+                        <a href="#"><button className="button">Details</button></a>
+                        <i className="fas fa-heart"></i> <span> 4</span>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default Categories
