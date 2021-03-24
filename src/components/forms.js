7d20784ea02acae6488d7f0e4b36d39b7b71746e
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-03-24 23:41:33 +0200

    Create forms.js

---
+++ src/components/forms.js
@@ -0,0 +1,15 @@
+const Form = () => {
+    return (
+        <div>
+            <h1>Demo js</h1>
+
+            <form>
+                <input type="text" name="username" />
+                <input type="number" name="age" />
+                <input type="button" value="Send" />
+            </form>
+        </div>
+    )
+}
+
+export default Form
