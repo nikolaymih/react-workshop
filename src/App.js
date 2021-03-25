23b18dadb84d96cc93345c51015a6fbc3f52ba43
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-03-25 08:59:18 +0200

    Forms Demo
    Short forms demo until 1h

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import Categories from './components/Categories/Categories';
 import PetsDetails from './components/PetsDetails/PetsDetails'
 import Footer from './components/Footer/Footer'
+import Demo from './components/forms'
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' exact component={Categories} />
         <Route path='/categories/:category' exact component={Categories} />
         <Route path='/pets/details/:petID' component={PetsDetails} />
+        <Route path='/demo' component={Demo} />
       </Switch>
 
       <Footer />
--- src/components/forms.js
+++ src/components/forms.js
@@ -1,15 +1,38 @@
-const Form = () => {
-    return (
-        <div>
-            <h1>Demo js</h1>
+import { Component } from 'react';
 
-            <form>
-                <input type="text" name="username" />
-                <input type="number" name="age" />
-                <input type="button" value="Send" />
-            </form>
-        </div>
-    )
+class Form extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: ''
+        }
+    }
+    submitHandler(e){
+        e.preventDefault();
+
+        console.log(e.target.username.value);
+    }
+
+    onChangeHandler(e) {
+        this.setState({username: e.target.value})
+    }
+    render() {
+        return (
+            <div>
+                <h1>Demo js</h1>
+    
+                <form onSubmit={this.submitHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" value={this.state.username} onChange={this.onChangeHandler.bind(this)} />
+                    <label htmlFor="age">Age</label>
+                    <input type="number" id="age" name="age" defaultValue="24" />
+                    <input type="submit" value="Send" />
+                    <button>Click me</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Form
