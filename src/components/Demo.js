4eea2face153ecd2bb54987f1828acecaaed1c00
nikolaymih <71232830+nikolaymih@users.noreply.github.com>
2021-03-30 16:19:03 +0300

    Update demo.js
    hooks

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,16 +1,53 @@
-
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Demo = () => {
+    // const [state, setState] = useState({
+    //     count: 11,
+    //     step: 1
+    // });
+
+    const [count, setCount] = useState(1);
+    const [step, setStep] = useState(1);
+    const [character, setCharacter] = useState({ name: 'Nobody' })
+
+    useEffect(() => {
+        fetch(`https://swapi.dev/api/people/${step}`)
+            .then(res => res.json())
+            .then(data => setCharacter(data))
+
+        return () => {
+            console.log('Component will unmount');
+        }
+    }, [step])
 
     const onCounterButtonClickHandler = () => {
-        console.log('Increase');
+        // setState(oldState => ({...oldState, count: oldState.count + oldState.step}))
+
+        setCount(prevState => prevState + step)
+    }
+
+    const onChangeStepSelectHandler = (e) => {
+        const stepValue = Number(e.target.value)
+
+        setStep(stepValue)
     }
 
     return (
         <div>
-            <h1>Counter</h1>
-            <div>0</div>
+            <h1>{character.name}'s Counter</h1>
+            <div>{count}</div>
             <button onClick={onCounterButtonClickHandler}>Icrement</button>
+            <label htmlFor="step"></label>
+            <select name="step" id="step" onChange={onChangeStepSelectHandler}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+            <div>
+                <Link to='/'>Home</Link>
+            </div>
         </div>
     )
 }
